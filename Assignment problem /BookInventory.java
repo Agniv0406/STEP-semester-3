64ff8083e8f8509c59bb9941b1052570e04be66f
Agniv Mukherjee <agniv0406@gmail.com>
2026-09-19 09:42:26 +0530

    Update BookInventory.java

--- Assignment problem /BookInventory.java
+++ Assignment problem /BookInventory.java
@@ -1,1 +1,67 @@
+package Week5.AssignmentProblems;
 
+public class BookInventory {
+
+    private final int copiesTotal;
+    private int copiesAvailable;
+
+    public BookInventory(int copiesTotal) {
+
+        if (copiesTotal < 0) {
+            copiesTotal = 0;
+        }
+
+        this.copiesTotal = copiesTotal;
+        this.copiesAvailable = copiesTotal;
+    }
+
+
+    public void checkOut() {
+
+        // Nothing available
+        if (copiesAvailable <= 0) {
+            return;
+        }
+
+        copiesAvailable--;
+    }
+
+
+    public void checkIn() {
+
+        // Already at full capacity
+        if (copiesAvailable >= copiesTotal) {
+            return;
+        }
+
+        copiesAvailable++;
+    }
+
+
+    public int getCopiesAvailable() {
+        return copiesAvailable;
+    }
+
+
+    public static void main(String[] args) {
+
+        BookInventory b = new BookInventory(3);
+
+        b.checkOut();
+        b.checkOut();
+        b.checkOut();
+        b.checkOut();
+
+        System.out.println(b.getCopiesAvailable());
+
+
+        BookInventory b2 = new BookInventory(3);
+
+        b2.checkIn();
+        b2.checkIn();
+        b2.checkIn();
+        b2.checkIn();
+
+        System.out.println(b2.getCopiesAvailable());
+    }
+}
